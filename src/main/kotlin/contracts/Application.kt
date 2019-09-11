d4fd7df1d2c5f813be54e4318aecc58ea17ed01e
Jesus Hernando Sancha <jesushs80@gmail.com>
2019-09-11 19:09:32 +0200

    Project scaffolding

---
+++ src/main/kotlin/contracts/Application.kt
@@ -0,0 +1,14 @@
+package contracts
+
+import io.micronaut.runtime.Micronaut
+
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("contracts")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
