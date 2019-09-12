1b99a2fb27b67a56b224b01f41156c82ef010328
Jesus Hernando Sancha <jesushs80@gmail.com>
2019-09-12 19:29:35 +0200

    Add consumer drive contract verification test

---
+++ src/test/kotlin/contracts/PactVerificationTest.kt
@@ -0,0 +1,37 @@
+package contracts
+
+import au.com.dius.pact.provider.junit.Provider
+import au.com.dius.pact.provider.junit.State
+import au.com.dius.pact.provider.junit.loader.PactFolder
+import au.com.dius.pact.provider.junit.target.TestTarget
+import au.com.dius.pact.provider.junit5.HttpTestTarget
+import au.com.dius.pact.provider.junit5.PactVerificationContext
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider
+import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.api.extension.ExtendWith
+import java.net.URL
+
+@MicronautTest
+@Provider("Contracts")
+@PactFolder("pacts")
+class PactVerificationTest {
+    @TestTarget
+    val target: HttpTestTarget = HttpTestTarget("localhost", 8090)
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider::class)
+    fun pactsVerificationTemplate(context: PactVerificationContext) {
+        context.verifyInteraction()
+    }
+
+    @BeforeEach
+    fun setupContext(context: PactVerificationContext) {
+        context.target = HttpTestTarget.fromUrl(URL("http://localhost:8090"))
+    }
+
+    @State("Get player's current contract")
+    fun testGetPayerCurrentContractInfo() {
+    }
+}
